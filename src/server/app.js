8528aad97dd8a6c75e25319d325d4d854f9ebe6b
Axel Ulmestig <axel.ulmestig@gmail.com>
2017-02-11 16:08:26 +0000

    changed references to index.html

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 app.use(express.static(__dirname + '/../../'));
-app.set('views', __dirname + '/../../');
+app.set('views', __dirname + '/../web/');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => res.render('src/web/index.html'));
+app.get('/', (req, res) => res.render('index.html'));
 
 app.listen(1337, () => console.log("server starting"));
