6ef33ad8e09de32a0440f15b1dc0a35e141c46f8
Axel Ulmestig <axel.ulmestig@gmail.com>
2017-02-11 15:42:10 +0000

    reorganized code file structure

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
 var app = express();
-app.use(express.static(__dirname + '/'));
-app.set('views', __dirname + '/');
+app.use(express.static(__dirname + '/../../'));
+app.set('views', __dirname + '/../../');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
-        res.render('index.html');
+        res.render('src/web/index.html');
 });
 
 app.listen(1337, function() {
